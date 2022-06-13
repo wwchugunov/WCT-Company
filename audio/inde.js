0830569c3b1cb582e3eec42c100d93c05beb51f9
Chugunov <w.w.chugunov@gmail.com>
2022-06-13 12:18:02 +0300

    fix-audio-1

--- audio/inde.js
+++ audio/inde.js
@@ -30,3 +30,37 @@ function playSong(){
    Progress.style.width = `${progressProcent}%`
  }
  audio.addEventListener('timeupdate', updateProgress)
+
+
+ const player1 = document.querySelector('.play-list1'); //Плеер
+const audio1 = document.querySelector('.audio1'); //Аудио
+const play1 = document.querySelector('.play-btn1'); // Кнопка
+const ProgressContainer1 = document.querySelector('.ProgressContainer1'); //Прогрес бар
+const Progress1 = document.querySelector('.Progress1'); //Прогрес бар (Полоса)
+
+
+function playSong1(){
+   play1.classList.add('playsong1')
+   audio.play()
+ }
+ 
+ function pauseSong1(){
+   play1.classList.remove('playsong1')
+   audio.pause()
+ }
+ 
+ play1.addEventListener('click', () => {
+   const isPlaying = play1.classList.contains('playsong1')
+   if(isPlaying){
+     pauseSong1()
+   }else{
+     playSong1()
+   }
+ })
+ 
+ function updateProgress1(e) {
+   const {duration, currentTime} = (e.srcElement)
+   const progressProcent = (currentTime / duration ) * 100
+   Progress1.style.width = `${progressProcent}%`
+ }
+ audio1.addEventListener('timeupdate', updateProgress)
